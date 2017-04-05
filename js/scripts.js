5b8e8a593c62d5bd7b836057e513237ee8162324
JanWrotkowski <trojmiasto2@wp.pl>
2017-04-06 00:54:55 +0200

    mala zmiana

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 
 
 function Pet(name, breed, color) {
-	this.Name = name;
+	this.name = name;
 	this.breed = breed;
 	this.color = color;
 }
 
 Pet.prototype.showInfo = function() {
-	console.log("Zwierz na obrazku to " + this.Name + " , domownik rasy " + this.breed + ", a jego kolor to " + this.color + " w odcieniu alfa." );                                                                                                                                                           
+	console.log("Zwierz na obrazku to " + this.name + " , domownik rasy " + this.breed + ", a jego kolor to " + this.color + " w odcieniu alfa." );                                                                                                                                                           
 }
 
 var Pies = new Pet("Stefan", "postrach kotow", "czarny braz");
